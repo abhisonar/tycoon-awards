72de5d2f1a87b6dea9dd2f3875a18b49a80a0a22
Ishita Gupta <ishitagupta0203@gmail.com>
2024-03-07 01:33:03 +0530

    chore{home-page} : unlock-opportunites-data

--- app/home/(unlock-opportunities)/unlock-opportunities.component.tsx
+++ app/home/(unlock-opportunities)/unlock-opportunities.component.tsx
@@ -2,7 +2,28 @@ import { UNLOCK_OPPORTUNITIES_DATA } from "@shared/data/unlock-opportunities.dat
 import React from "react";
 
 const UnlockOpportunitiesComponent = () => {
-  return <div>LSJGSDGNKDBGKBDFB</div>;
+  return (
+    <div className="flex flex-col mt-4 items-center gap-4">
+      <h2 className="text-center text-[.7rem] font-semibold md:text-md lg:text-xl pb-1 border-b-2 border-b-primary-darker w-fit"></h2>
+      <h1 className="text-center text-lg font-bold md:text-xl lg:text-2xl">
+        Unlocking Opportunities for Your Business
+      </h1>
+      <div className="flex flex-col md:flex-row items-center justify-center flex-wrap w-full p-3 gap-3">
+        {UNLOCK_OPPORTUNITIES_DATA.map((item, index) => (
+          <div
+            key={item.title}
+            className="flex flex-col p-3 bg-white w-full md:basis-[45%]  lg:basis-[30%] shadow-md overflow-hidden"
+          >
+            <div className="h-[170px] lg:h-[200px] object-contain group-hover:scale-110 transition-all duration-300" />
+            <h3 className="text-lg font-bold text-center text-primary-default">
+              {item.title}
+            </h3>
+            <p className="text-sm text-center">{item.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default UnlockOpportunitiesComponent;
