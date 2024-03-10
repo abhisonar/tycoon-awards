024896195447ff3a9a6802d41e6ed07663657e46
Ishita Gupta <ishitagupta0203@gmail.com>
2024-03-10 16:30:30 +0530

    chore ( data ) : nav-bar-data-entered

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const CategoryData = () => {
+  return <div>Category Data</div>;
 };
 
-export default page;
+export default CategoryData;
---
+++ app/services/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ServicePage = () => {
+  return <div>ServicesComponent</div>;
+};
+
+export default ServicePage;
