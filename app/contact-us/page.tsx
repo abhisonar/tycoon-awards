cdc435958dc2a560bfd8685966e53133b76bdcab
Abhishek Sonar <abhisonar4121@gmail.com>
2024-03-29 09:41:28 +0530

    chore(contactus): updated address

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import Head from "next/head";
+import React from 'react';
+import Head from 'next/head';
 
 const ContactUs = () => {
   return (
@@ -12,14 +12,10 @@ const ContactUs = () => {
           here to assist you.
         </p>
         <p className="text-white mb-2">
-          Address :
-          <span className="font-bold">
-            {" "}
-            Shop No 68, 2nd Floor, Jagtap Complex, Shramik Nagar, Satpur, Nashik-422012
-          </span>
+          Address : <span className="font-bold">Mumbai</span>
         </p>
         <p className="text-white mb-2">
-          Email :{" "}
+          Email :{' '}
           <a className="font-bold" href="mailto:hello@tycoonsofasia.com">
             hello@tycoonsofasia.com
           </a>
@@ -30,7 +26,7 @@ const ContactUs = () => {
       </div>
       <div className="h-96">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d234.33289533821662!2d73.71116912410007!3d19.99481428165237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdded0b95b003c5%3A0x9a5202de9f0f453!2sJagtap%20Commercial%20Complex!5e0!3m2!1sen!2sin!4v1710361159679!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d482634.93194140197!2d72.55150653640894!3d19.081957704971398!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c6306644edc1%3A0x5da4ed8f8d648c69!2sMumbai%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1711685223803!5m2!1sen!2sin"
           width="100%"
           height="100%"
           loading="lazy"
